cc10ed6971c0c52e6acb03e9bb7e6fe329f9ca92
Christian Batista <christian.dev88@gmail.com>
2026-08-04 17:16:51 -0300

    feat: ppp

--- src/main.c
+++ src/main.c
@@ -95,16 +95,16 @@ main(int argc, char *argv[])
 
 	print("vpnfs: tunnel active. System pipes posted at /srv/vpnfs.in and /srv/vpnfs.out\n");
 
-	/* Automatically spawn /bin/ppp daemon on the posted /srv/vpnfs pipes */
+	/* Automatically spawn /bin/ip/ppp daemon on the posted /srv/vpnfs pipes */
 	switch(rfork(RFPROC|RFFDG|RFNOTEG)){
 	case -1:
 		fprint(2, "vpnfs: warning: failed to fork ppp daemon: %r\n");
 		break;
 	case 0:
 		if(cfg.verbose)
-			print("vpnfs: starting /bin/ppp -m 1400 /srv/vpnfs.in /srv/vpnfs.out...\n");
-		execl("/bin/ppp", "ppp", "-m", "1400", "/srv/vpnfs.in", "/srv/vpnfs.out", nil);
-		fprint(2, "vpnfs: exec /bin/ppp failed: %r\n");
+			print("vpnfs: starting /bin/ip/ppp -P -m 1400 /srv/vpnfs.in /srv/vpnfs.out...\n");
+		execl("/bin/ip/ppp", "ppp", "-P", "-m", "1400", "/srv/vpnfs.in", "/srv/vpnfs.out", nil);
+		fprint(2, "vpnfs: exec /bin/ip/ppp failed: %r\n");
 		exits("exec ppp failed");
 	}
 
